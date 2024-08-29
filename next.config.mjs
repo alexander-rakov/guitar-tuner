9328f9c26d2ea5ea0e76cd0f1860c9444a5589ed
Alex Rakov <alexander-rakov@users.noreply.github.com>
2024-08-28 18:51:12 -0700

    Add deployment to gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/guitar-tuner'
+    basePath: '/guitar-tuner',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/guitar-tuner/' : '',
 };
 
 export default nextConfig;
